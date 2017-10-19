8500c53110963c6d09727dd9fcec4105aea5f500
Ollie <ollierahman@gmail.com>
2017-10-19 18:29:39 +0100

    Made the UI slightly nicer and easier to read

--- UI/Basic UI/UILayout.java
+++ UI/Basic UI/UILayout.java
@@ -17,30 +17,42 @@ public class UILayout {
     JFrame frame = new JFrame("Basic UI");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setResizable(false);
-    frame.setLayout(new GridLayout(5, 2, 5, 5));
+    frame.setLayout(new GridLayout(11, 3, 0, 5));
     
     
     frame.add(new JLabel("Title:"));
-    frame.add(new JTextField());
+    frame.add(new JTextField("", 15));
+    
     frame.add(new JLabel("First Name:"));
-    frame.add(new JTextField());
+    frame.add(new JTextField("", 15));
+    
     frame.add(new JLabel("Last Name:"));
-    frame.add(new JTextField());
+    frame.add(new JTextField("", 15));
+    
     frame.add(new JLabel("Date of Birth:"));
-    frame.add(new JTextField());
+    frame.add(new JTextField("", 15));
+    
     frame.add(new JLabel("Phone Number:"));
-    frame.add(new JTextField());
+    frame.add(new JTextField("", 15));
+    
     frame.add(new JLabel("House Number:"));
-    frame.add(new JTextField());
+    frame.add(new JTextField("", 15));
+    
     frame.add(new JLabel("Street Name:"));
-    frame.add(new JTextField());
+    frame.add(new JTextField("", 15));
+    
     frame.add(new JLabel("District Name:"));
-    frame.add(new JTextField());
+    frame.add(new JTextField("", 15));
+    
+    frame.add(new JLabel("City:"));
+    frame.add(new JTextField("", 15));
+    
     frame.add(new JLabel("Post Code:"));
     frame.add(new JTextField());
-    frame.add(new JLabel("City:"));
-    frame.add(new JTextField());
     
+    frame.add(new JLabel());
+    frame.add(new JButton("Enter"));
+   
     
 
     frame.pack();
