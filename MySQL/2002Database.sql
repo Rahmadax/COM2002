edbd2607b441b12020b076b8872dff15aa0668b6
Ollie <ollierahman@gmail.com>
2017-11-06 20:32:58 +0000

    Add files via upload

--- MySQL/2002Database.sql
+++ MySQL/2002Database.sql
@@ -4,51 +4,53 @@ CREATE TABLE Patient (
     LastName varchar(20)     NOT NULL,
     DOB date,
     ContactNumber varchar(7),
-	HouseNumber varchar(10)  NOT NULL,
-	Postcode varchar (10)    NOT NULL,
-	PRIMARY KEY (PatientID)
+    HouseNumber varchar(10)  NOT NULL,
+    Postcode varchar (10)    NOT NULL,
+    PRIMARY KEY (PatientID)
 );
 
 CREATE TABLE Address (
     HouseNumber varchar(10)  NOT NULL,
-	Postcode varchar(10)     NOT NULL,
-	StreetName varchar(15),
-	DistrictName varchar(15),
-	CityName varchar(15),
-	PRIMARY KEY (HouseNumber, Postcode)
+    Postcode varchar(10)     NOT NULL,
+    StreetName varchar(15),
+    DistrictName varchar(15),
+    CityName varchar(15),
+    PRIMARY KEY (HouseNumber, Postcode)
 );
 
 CREATE TABLE Appointment (
-	AppointmentDate date    NOT NULL,
-	StartTime time          NOT NULL,
-	Partner varchar(15)     NOT NULL, 
-	EndTime time,           NOT NULL
-	Empty boolean,          NOT NULL
-	PatientID int(9)        NOT NULL,
-	PRIMARY KEY (AppointmentDate, StartTime, Partner)
+    AppointmentDate date    NOT NULL,
+    StartTime time          NOT NULL,
+    Partner varchar(15)     NOT NULL, 
+    EndTime time,           NOT NULL
+    Empty boolean,          NOT NULL
+    PatientID int(9)        NOT NULL,
+    PRIMARY KEY (AppointmentDate, StartTime, Partner)
 );
 
 CREATE TABLE treatment(
-	TreatmentName varchar(25)      NOT NULL,
-	TreatmentCost decimal(10, 2),
-	AppointmentDate date           NOT NULL,
-	StartTime time                 NOT NULL,
-	Partner varchar(15)            NOT NULL,
-	PRIMARY KEY (TreatmentName)
+    TreatmentName varchar(25)      NOT NULL,
+    TreatmentCost decimal(10, 2)   NOT NULL,
+    AppointmentDate date           NOT NULL,
+    StartTime time                 NOT NULL,
+    Partner varchar(15)            NOT NULL,
+    PRIMARY KEY (TreatmentName)
 );
 
 CREATE TABLE Patient-HCP-LINKER (
-	PatientID int(8)      NOT NULL,
-	HCPName varchar(15)   NOT NULL
+    PatientID int(8)      NOT NULL,
+    HCPName varchar(15)   NOT NULL,
+	PRIMARY KEY (PatientID, HCPName)
 );
 
 CREATE TABLE HCP (
-	HCPName varchar(10)   NOT NULL,
-	MonthlyPayment int(10),
-	CheckupsMax int (2),
-	CheckupsTaken int(2),
-	HygeneMax int(2),
-	HygeneTaken int(2),
-	RepairsMax int(2),
-	RepairsTaken int(2)
+    HCPName varchar(10)   NOT NULL,
+    MonthlyPayment int(10),
+    CheckupsMax int (2),
+    CheckupsTaken int(2),
+    HygeneMax int(2),
+    HygeneTaken int(2),
+    RepairsMax int(2),
+    RepairsTaken int(2),
+	PRIMARY KEY (HCPName)
 );
