17db06afe4496087168e196f8f03c55544d13924
Aydin Hepsaydir <Aydin Hepsaydir@DESKTOP-QJC7F6R>
2017-11-17 23:00:40 +0000

    patient add and remove from table

--- src/mysql/query/PatientQuery.java
+++ src/mysql/query/PatientQuery.java
@@ -1,5 +1,7 @@
 package mysql.query;
 
+import java.sql.PreparedStatement;
+
 import mysql.MySQLAccess;
 
 public final class PatientQuery extends QuerySQL {
@@ -12,16 +14,46 @@ public final class PatientQuery extends QuerySQL {
 		
 	}
 	
-	public void add() {
+	public void add(int PatientID, String FirstName, String LastName, String DOB, int ContactNumber, int HouseNumber, String PostCode) throws Exception {
 		
+		int patID = PatientID;
+		String fName = FirstName;
+		String lName = LastName;
+		String dob = DOB;
+		int cNumber = ContactNumber;
+		int hNumber = HouseNumber;
+		String pCode = PostCode;
+		
+		try {
+			PreparedStatement insert = connect.prepareStatement("INSERT INTO Patient (PatientID, FirstName, LastName, DOB, ContactNumber, HouseNumber, PostCode) "
+					+ "VALUES '+patID+', '+fName+', '+lName+', SELECT CONVERT (date, '+dob+'), '+cNumber+', '+hNumber+', '+pCode+')"); 
+			insert.executeUpdate();
+		} catch (Exception e) {System.out.println(e);}
+		finally {
+			System.out.println("Insert Completed");
+		}
 	}
 
 	public void edit() {
 		
 	}
 
-	public void remove() {
+	public void remove(int PatientID) throws Exception {
 		
+		int patID = PatientID; 
+		
+		try {
+			PreparedStatement delete = connect.prepareStatement("DELETE FROM Patient"
+					+ "WHERE PatientID = ?");
+			
+			delete.setInt(1, patID);
+			
+			delete.executeUpdate();
+			
+		} catch (Exception e) {System.out.println(e);}
+		finally {
+			System.out.println("Deletion Completed");
+		}
 	}
 
 }
