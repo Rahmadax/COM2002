857c62f2207b9047a84f662781a249be4e95cf67
Aydin Hepsaydir <arphepsaydir1@sheffield.ac.uk>
2017-11-25 13:13:09 +0000

    Update AddressQuery.java

--- src/mysql/query/AddressQuery.java
+++ src/mysql/query/AddressQuery.java
@@ -1,26 +1,34 @@
 package mysql.query;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import mysql.MySQLAccess;
 
 public class AddressQuery extends QuerySQL {
 	
-	protected AddressQuery(MySQLAccess access) {
+	public AddressQuery(MySQLAccess access) {
 		super(access);
 	}
 	
+	public ResultSet get(int houseNumber, String postCode) throws Exception {
+		PreparedStatement get = connect.prepareStatement("SELECT HouseNumer Postcode FROM Address "
+				+ "HouseNumber = " +houseNumber+ " and Postcode = " +postCode+ ";");
+		return resultSet;
+		
+		
+		
+	}
 	public void add(int houseNumber, String postCode, String streetName, String districtName, String cityName) {
 		
 		try {
-			PreparedStatement useDB = connect.prepareStatement("USE team031;");
 			PreparedStatement insert = connect.prepareStatement("INSERT INTO Address VALUES ("
 					+houseNumber+ ", '" +postCode+ "', '" +streetName+ "', '" +districtName+ "', '" +cityName+ "');");
 			
-			useDB.executeUpdate();
 			insert.executeUpdate();
 			
 		} catch(Exception e) {System.out.println(e);}
 	}
-
+	
+	
 }
