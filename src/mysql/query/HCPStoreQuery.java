28e6e5a08ad2762b7a2ec648f2d802b962a4088a
Aydin Hepsaydir <arphepsaydir1@sheffield.ac.uk>
2017-11-25 19:26:02 +0000

    Add files via upload
    query for health care plans (get all plans)

---
+++ src/mysql/query/HCPStoreQuery.java
@@ -0,0 +1,36 @@
+package mysql.query;
+
+import mysql.MySQLAccess;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class HCPStoreQuery extends QuerySQL {
+	
+	protected HCPStoreQuery(MySQLAccess access) {
+		super(access);
+	}
+	
+	public String[] getAll() throws Exception {
+		PreparedStatement getAll = prepareStatement("SELECT * FROM HCPStore;");
+
+		ResultSet rs = getAll.executeQuery();
+		
+        int rows = getRowCount(rs);
+        String[] HCPStore = new String[rows];
+        
+        while (rs.next()) {
+            int currRow = rs.getRow() - 1;
+
+            String HCPName = rs.getString(1);
+            BigDecimal monthPay = rs.getBigDecimal(2);
+            int checkupMax = rs.getInt(3);
+            int hygieneMax = rs.getInt(4);
+            int repairsMax = rs.getInt(5);
+            
+            HCPStore[currRow] = HCPName + " (" +String.format("%.2f", monthPay)+ ", " +checkupMax+ ", " +hygieneMax+ ", " +repairsMax+ ")";
+        }
+		return HCPStore;
+	}
+}
