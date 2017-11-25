507097dc18dab13a3285d34bb327d79a5b9a4f0b
Aydin Hepsaydir <arphepsaydir1@sheffield.ac.uk>
2017-11-25 17:36:02 +0000

    Update AddressQuery.java
    removed get method as did not work

--- src/mysql/query/AddressQuery.java
+++ src/mysql/query/AddressQuery.java
@@ -11,25 +11,6 @@ public class AddressQuery extends QuerySQL {
 		super(access);
 	}
 	
-	public String get(int houseNumber, String postCode) throws Exception {
-		PreparedStatement get = prepareStatement("SELECT HouseNumer, Postcode FROM Address WHERE "
-				+ "HouseNumber = ? and Postcode = ?;");
-		preparedStatement.setInt(1, houseNumber);
-		preparedStatement.setString(2, postCode);
-		
-		resultSet = preparedStatement.executeQuery();
-		
-		int hn = 0;
-		String pc = null;
-		
-		while (resultSet.next()) {
-			hn = resultSet.getInt(1);
-			pc = resultSet.getString(2);
-		}
-		close();
-		return hn.toString() + pc;
-		
-	}
 	public void add(int houseNumber, String postCode, String streetName, String districtName, String cityName) {
 		
 		try {
