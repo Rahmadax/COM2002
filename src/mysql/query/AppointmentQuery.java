accae73fee756f9591bf25bc7fb97006d16e0951
Ollie <ollierahman@gmail.com>
2017-11-25 17:31:03 +0000

    Updated delete appointment

--- src/mysql/query/AppointmentQuery.java
+++ src/mysql/query/AppointmentQuery.java
@@ -64,18 +64,96 @@ public class AppointmentQuery extends QuerySQL {
 		close();
 	}
 	
-	public void remove(int patID, String appDate) throws Exception {
-		
-		try {
-			PreparedStatement delete = connect.prepareStatement("DELETE FROM Appointments"
-					+ "WHERE PatientID = "+patID+ " and AppointmentDate = '"+appDate+"');");
-			
-			delete.executeUpdate();
-			close();
-			
-		} catch (Exception e) {System.out.println(e);}
+	package mysql.query;
 
+import java.sql.PreparedStatement;
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+
+import calendar.Appointment;
+import mysql.MySQLAccess;
+
+public class AppointmentQuery extends QuerySQL {
+
+	public AppointmentQuery(MySQLAccess access) {
+		super(access);
 	}
+
+	public Appointment[] get(Date date) throws Exception {
+		preparedStatement = prepareStatement(
+				"SELECT StartTime, Partner, EndTime, PatientID "
+				+ "FROM Appointments WHERE AppointmentDate = ?;");
+		preparedStatement.setDate(1, new java.sql.Date(date.getTime()));
+		resultSet = preparedStatement.executeQuery();
+		
+		int rows = getRowCount(resultSet);
+		Appointment[] appointments = new Appointment[rows];
+		
+		while (resultSet.next()) {
+			int currRow = resultSet.getRow() - 1;
+			
+			Time startTime = resultSet.getTime(1);
+			String partner = resultSet.getString(2);
+			Time endTime = resultSet.getTime(3);
+			int patientID = resultSet.getInt(4);
+
+			appointments[currRow] = new Appointment(
+					createDate(date, startTime), createDate(date, endTime), 
+					partner, patientID);
+		}
+
+		close();
+		
+		return appointments;
+	}
+
+	public void add(HashMap<String, String> map) throws Exception {
+		String appDate = (String) map.get("AppointmentDate");
+		String startTime = (String) map.get("StartTime");
+		String partner = (String) map.get("Partner");
+		String endTime = (String) map.get("EndTime");
+		int patID = Integer.parseInt(map.get("PatientID"));
+		
+		preparedStatement = prepareStatement("INSERT INTO Appoitment (AppointmentDate, StartTime, Partner, EndTime, Empty, PatientID) "
+				+ "VALUES (SELECT CONVERT(date, ?), SELECT CONVERT(time, ?), '+partner+', SELECT CONVERT(time, ?), ?, ?)"); 
+		preparedStatement.setString(1, appDate);
+		preparedStatement.setString(2, startTime);
+		preparedStatement.setString(3, partner);
+		preparedStatement.setString(4, endTime);
+		preparedStatement.setInt(5, patID);
+		
+		preparedStatement.executeUpdate();
+		
+		close();
+	}
+	
+	public void remove(Date appDate, Time startTime, String partner) throws Exception {
+
+		preparedStatement = prepareStatement("DELETE FROM Appointments WHERE (AppointmentDate, startTime, Partner) = " +
+				"VALUES (?,?,?);");
+		preparedStatement.setDate(1, (java.sql.Date) appDate);
+		preparedStatement.setTime(2, startTime);
+		preparedStatement.setString(3, partner);
+        	preparedStatement.executeQuery();
+	}
+	
+	private Date createDate(Date date, Time time) {
+		Calendar timeC = Calendar.getInstance(Locale.UK);
+		timeC.setTime(time);
+		
+		Calendar calendar = Calendar.getInstance(Locale.UK);
+		calendar.setTime(date);
+		calendar.set(Calendar.HOUR_OF_DAY, timeC.get(Calendar.HOUR_OF_DAY));
+		calendar.set(Calendar.MINUTE, timeC.get(Calendar.MINUTE));
+
+		return calendar.getTime();
+	}
+	
+}
+	
 	
 	private Date createDate(Date date, Time time) {
 		Calendar timeC = Calendar.getInstance(Locale.UK);
