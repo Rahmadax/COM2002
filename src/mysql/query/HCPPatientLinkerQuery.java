21c3da774510a26293fc22c8aeeb4d81e0f07dcd
Ollie <ollierahman@gmail.com>
2017-11-27 05:58:03 +0000

    Add files via upload

--- src/mysql/query/HCPPatientLinkerQuery.java
+++ src/mysql/query/HCPPatientLinkerQuery.java
@@ -11,14 +11,28 @@ public class HCPPatientLinkerQuery extends QuerySQL {
 	public HCPPatientLinkerQuery(MySQLAccess access) {
 		super(access);
 	}
-	public int getHCPID(int patientID) throws Exception {
-		String query = "SELECT HCPID FROM Patients WHERE PatientID =" + patientID + ";";		 
+	public int[] getHCPDetails(int patientID) throws Exception {
+		String query = "SELECT HCPID FROM HCPPatient_Linker WHERE PatientID =" + patientID + ";";
 	     preparedStatement = prepareStatement(query); 
 		 resultSet = preparedStatement.executeQuery(query);
-		 int returnInt = resultSet.getInt(1);		 
+         int hcpID = 0;
+		 while (resultSet.next())
+		 	hcpID = resultSet.getInt(1);
+
+		preparedStatement = prepareStatement("SELECT * FROM HCPs WHERE "
+				+ "HCPID = ?;");
+		preparedStatement.setInt(1, hcpID);
+		resultSet = preparedStatement.executeQuery();
+		int[] list = new int[4];
+		while(resultSet.next()){
+			list[0] = resultSet.getInt(1);
+			list[1] = resultSet.getInt(4);
+			list[2] = resultSet.getInt(5);
+			list[3] = resultSet.getInt(6);
+		}
 		close();
-		return returnInt;
-	}
+		return list;
+		}
 
 	
 }
